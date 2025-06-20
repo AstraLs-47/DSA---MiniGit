f4f1927a50ece04be20245a2e1ca88b32cad06d7
Amiel725 <amiel5166@gmail.com>
2025-06-20 05:36:35 +0300

    Update MiniGit.hpp

--- MiniGit.hpp
+++ MiniGit.hpp
@@ -1,108 +1,39 @@
-#include <iostream>
-
-void printArray(int *array, int n)
-{
-    for (int i = 0; i < n; ++i)
-        std::cout << array[i] << std::endl;
-}
-
-void quickSort(int *array, int low, int high)
-{
-    int i = low;
-    int j = high;
-    int pivot = array[(i + j) / 2];
-    int temp;
-
-    while (i <= j)
-    {
-        while (array[i] < pivot)
-            i++;
-        while (array[j] > pivot)
-            j--;
-        if (i <= j)
-        {
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            i++;
-            j--;
-        }
-    }
-    if (j > low)
-        quickSort(array, low, j);
-    if (i < high)
-        quickSort(array, i, high);
-}
-
-int main()
-{
-    int array[] = {95, 45, 48, 98, 1, 485, 65, 478, 1, 2325};
-    int n = sizeof(array)/sizeof(array[0]);
-
-    std::cout << "Before Quick Sort :" << std::endl;
-    printArray(array, n);
-
-    quickSort(array, 0, n);
-
-    std::cout << "After Quick Sort :" << std::endl;
-    printArray(array, n);
-    return (0);
-}
-// C++ Program to demonstrate how to implement the quick
-// sort algorithm
-#include <bits/stdc++.h>
-using namespace std;
-
-int partition(vector<int> &vec, int low, int high) {
-
-    // Selecting last element as the pivot
-    int pivot = vec[high];
-
-    // Index of elemment just before the last element
-    // It is used for swapping
-    int i = (low - 1);
-
-    for (int j = low; j <= high - 1; j++) {
-
-        // If current element is smaller than or
-        // equal to pivot
-        if (vec[j] <= pivot) {
-            i++;
-            swap(vec[i], vec[j]);
-        }
-    }
-
-    // Put pivot to its position
-    swap(vec[i + 1], vec[high]);
-
-    // Return the point of partition
-    return (i + 1);
-}
-
-void quickSort(vector<int> &vec, int low, int high) {
-
-    // Base case: This part will be executed till the starting
-    // index low is lesser than the ending index high
-    if (low < high) {
-
-        // pi is Partitioning Index, arr[p] is now at
-        // right place
-        int pi = partition(vec, low, high);// Separately sort elements before and after the
-        // Partition Index pi
-        quickSort(vec, low, pi - 1);
-        quickSort(vec, pi + 1, high);
-    }
-}
-
-int main() {
-    vector<int> vec = {10, 7, 8, 9, 1, 5};
-    int n = vec.size();
- 
-   // Calling quicksort for the vector vec
-    quickSort(vec, 0, n - 1);
-
-    for (auto i : vec) {
-        cout << i << " ";
-    }
-    return 0;
-}
+#pragma once
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+struct Blob {    
+  std::string hash;
+  std::string filename;
+};
+struct Commit {
+  std::string hash;
+  std::string message;
+  std::string timestamp;
+  std::string parent;
+  std::vector<Blob> blobs;
+};
+class MiniGit {
+public:
+  MiniGit();
+  void init();
+  void add(const std::string& filename);
+  void commit(const std::string& message);
+  void log() const;
+  void branch(const std::string& branchName);
+  void checkout(const std::string& ref);
+  void status() const;
+  void merge(const std::string& branchName);
+  void diff(const std::string& commit1, const std::string& commit2);
+  void show(const std::string& filename) const;
+private:
+  std::string head;
+  std::map<std::string, Commit> commits; 
+  std::map<std::string, std::string> branches;
+  std::set<std::string> stagedFiles; 
+  void saveBlob(const std::string& filename, const std::string& hash);
+  std::string getTimestamp() const;
+  void save(); 
+  void load();
+};
